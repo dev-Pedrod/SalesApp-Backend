9da87e0b4abc70ca8e4abf85325cdb4a021f728c
dev-Pedrod <pedroolivxira@gmail.com>
2021-10-12 18:18:30 -0300

    Pedido, EstadoPagamento e pagamento

--- src/main/java/com/devpedrod/cursomc/CursomcApplication.java
+++ src/main/java/com/devpedrod/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.devpedrod.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.devpedrod.cursomc.domain.Cidade;
 import com.devpedrod.cursomc.domain.Cliente;
 import com.devpedrod.cursomc.domain.Endereco;
 import com.devpedrod.cursomc.domain.Estado;
+import com.devpedrod.cursomc.domain.Pagamento;
+import com.devpedrod.cursomc.domain.PagamentoComBoleto;
+import com.devpedrod.cursomc.domain.PagamentoComCartao;
+import com.devpedrod.cursomc.domain.Pedido;
 import com.devpedrod.cursomc.domain.Produto;
+import com.devpedrod.cursomc.domain.enums.EstadoPagamento;
 import com.devpedrod.cursomc.domain.enums.TipoCliente;
 import com.devpedrod.cursomc.repositories.CategoriaRepository;
 import com.devpedrod.cursomc.repositories.CidadeRepository;
 import com.devpedrod.cursomc.repositories.ClienteRepository;
 import com.devpedrod.cursomc.repositories.EnderecoRepository;
 import com.devpedrod.cursomc.repositories.EstadoRepository;
+import com.devpedrod.cursomc.repositories.PagamentoRepository;
+import com.devpedrod.cursomc.repositories.PedidoRepository;
 import com.devpedrod.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -75,7 +87,7 @@ public class CursomcApplication implements CommandLineRunner {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c2));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-		
+	
 		Endereco e1 = new Endereco(null, "Rua flores","300" , "apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos","105" , "Sala 800", "Centro", "38777012", cli1, c2);
 		
@@ -85,5 +97,21 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		
+		ped2.setPagamento(pagto2);
+		ped1.setPagamento(pagto1);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 }
--- src/main/java/com/devpedrod/cursomc/domain/enums/EstadoPagamento.java
+++ src/main/java/com/devpedrod/cursomc/domain/enums/EstadoPagamento.java
@@ -1,14 +1,15 @@
 package com.devpedrod.cursomc.domain.enums;
 
-public enum TipoCliente {
-
-	PESSOAFISICA(1, "Pessoa Física"),
-	PESSOAJURIDICA(2, "Pessoa Jurídica");
-
+public enum EstadoPagamento {
+	
+	PENDENTE(1, "Pendente."),
+	QUITADO(2, "Quitado"),
+	CANCELADO(3, "Cancelado");
+	
 	private int id;
 	private String descricao;
 
-	private TipoCliente(int id, String descricao) {
+	private EstadoPagamento(int id, String descricao) {
 		this.id = id;
 		this.descricao = descricao;
 	}
@@ -21,13 +22,13 @@ public enum TipoCliente {
 		return descricao;
 	}
 
-	public static TipoCliente toEnum(Integer id) {
+	public static EstadoPagamento toEnum(Integer id) {
 
 		if (id == null) {
 			return null;
 		}
 
-		for (TipoCliente x : TipoCliente.values()) {
+		for (EstadoPagamento x : EstadoPagamento.values()) {
 			if (id.equals(x.getId())) {
 				return x;
 			}

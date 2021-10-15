a7d040e46fd06f437bfbd6d2956eb8864a360480
dev-Pedrod <pedroolivxira@gmail.com>
2021-10-15 18:09:05 -0300

    validacao sintatica com Bean Validation

--- src/main/java/com/devpedrod/cursomc/services/CategoriaService.java
+++ src/main/java/com/devpedrod/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.devpedrod.cursomc.domain.Categoria;
+import com.devpedrod.cursomc.dto.CategoriaDTO;
 import com.devpedrod.cursomc.repositories.CategoriaRepository;
 import com.devpedrod.cursomc.services.exceptions.DataIntegrityException;
 import com.devpedrod.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), 
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }

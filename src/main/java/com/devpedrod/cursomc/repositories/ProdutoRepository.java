8074539bd82b82cb7200e26ca4e0e28a038e47bb
dev-Pedrod <pedroolivxira@gmail.com>
2021-10-19 08:44:25 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/devpedrod/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/devpedrod/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,19 @@
 package com.devpedrod.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devpedrod.cursomc.domain.Categoria;
 import com.devpedrod.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingIgnoreCaseAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }

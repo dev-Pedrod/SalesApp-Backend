270e318d69408bf610e9a8d1e8089392b3644df0
dev-Pedrod <pedroolivxira@gmail.com>
2021-10-22 09:23:56 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/devpedrod/cursomc/services/DBService.java
+++ src/main/java/com/devpedrod/cursomc/services/DBService.java
@@ -111,7 +111,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c2));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "nt291874@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 	
 		Endereco e1 = new Endereco(null, "Rua flores","300" , "apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos","105" , "Sala 800", "Centro", "38777012", cli1, c2);

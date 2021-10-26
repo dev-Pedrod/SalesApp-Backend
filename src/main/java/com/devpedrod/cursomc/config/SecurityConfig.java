39b16a9cf8206ae190d6229e9d5dbd2489e51269
dev-Pedrod <pedroolivxira@gmail.com>
2021-10-26 15:40:45 -0300

    Implementando autorização

--- src/main/java/com/devpedrod/cursomc/config/SecurityConfig.java
+++ src/main/java/com/devpedrod/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.devpedrod.cursomc.security.JWTAuthenticationFilter;
+import com.devpedrod.cursomc.security.JWTAuthorizationFilter;
 import com.devpedrod.cursomc.security.JWTUtil;
 
 @Configuration
@@ -54,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	

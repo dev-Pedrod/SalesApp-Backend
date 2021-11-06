47b94fcc064f3d1c65effd6fd8c3264f6600691b
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-06 12:24:45 -0300

    Enviando imagem via endpoint

--- src/main/java/com/devpedrod/cursomc/CursomcApplication.java
+++ src/main/java/com/devpedrod/cursomc/CursomcApplication.java
@@ -1,17 +1,11 @@
 package com.devpedrod.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.devpedrod.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -20,6 +14,5 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		s3Service.uploadFile("C:\\Users\\pxdro\\OneDrive\\Área de Trabalho\\imagens\\teste.jpg");
 	}
 }
